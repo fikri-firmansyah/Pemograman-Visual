f1cd3dcb9183ab3211cd85c6be26b3a81472aafe
fikri-firmansyah <62703555+fikri-firmansyah@users.noreply.github.com>
2020-03-26 21:28:32 +0700

    Add files via upload

---
+++ kelasutama.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelasutama;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author mac
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+       //System.out.println("Hallo dunia");
+       //menghitung objek
+       //frameutama futama = new frameutama();
+     // futama.tampil("Program Pertamaku");
+   // futama.tampil();
+    
+   // futama2 ut= new futama2();
+  //  ut.setVisible(true);
+        JOptionPane.showMessageDialog(null, "Selamat Belajar Provis");
+    }
+    
+}
